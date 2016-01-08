c0961c940a33c5ecc6ece196d92c0c324c2dff85
Sandra <72sandraloga@gmail.com>
2016-01-08 14:06:57 +0100

    Añadida nueva clase Usuario

---
+++ Usuario.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Usuario here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Usuario
+{
+    // instance variables - replace the example below with your own
+    private String nombre;
+    private String alimento:
+    private float gramos;
+    /**
+     * Constructor for objects of class Usuario
+     */
+    public Usuario(String nombre)
+    {
+        // initialise instance variables
+        this.nombre = nombre;
+        this.alimento = alimento;
+        this.gramos = gramos;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void comer(Alimento alimento,float gramos)
+    {
+        // put your code here
+        this.gramos=gramos;
+    }
+}
